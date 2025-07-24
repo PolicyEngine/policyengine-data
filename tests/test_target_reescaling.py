a3122ee1c6b5e19479bd679ad73bfc0b7f93b3b6
juaristi22 <juaristi@uni.minerva.edu>
2025-07-24 16:53:50 +0200

    leave placeholder in test for when database is published to the cloud

--- tests/test_target_reescaling.py
+++ tests/test_target_reescaling.py
@@ -7,22 +7,25 @@ def test_rescale_calibration_targets_from_db() -> None:
     from policyengine_data.calibration import rescale_calibration_targets
 
     # Local SQLite database Ben created
-    db_uri = "sqlite:////Users/movil1/Desktop/PYTHONJOBS/PolicyEngine/policy_data.db"
+    local_file = "sqlite:////Users/movil1/Desktop/PYTHONJOBS/PolicyEngine/policy_data.db"
+    # Placeholder for actual database URI once it is published from when merged into us-data
+    db_uri = None
 
-    results = rescale_calibration_targets(db_uri=db_uri)
+    if db_uri is not None:
+        results = rescale_calibration_targets(db_uri=db_uri)
 
-    assert results.columns.tolist() == [
-        "target_id",
-        "stratum_id",
-        "stratum_group_id",
-        "parent_stratum_id",
-        "variable",
-        "period",
-        "reform_id",
-        "value",
-        "scaled_value",
-        "scaling_factor",
-    ], "Missing columns for rescaled targets and scaling factors"
-    assert results.value.equals(
-        results.scaled_value
-    ), "Values and scaled values should match for age targets as state and national values matched"
+        assert results.columns.tolist() == [
+            "target_id",
+            "stratum_id",
+            "stratum_group_id",
+            "parent_stratum_id",
+            "variable",
+            "period",
+            "reform_id",
+            "value",
+            "scaled_value",
+            "scaling_factor",
+        ], "Missing columns for rescaled targets and scaling factors"
+        assert results.value.equals(
+            results.scaled_value
+        ), "Values and scaled values should match for age targets as state and national values matched"
