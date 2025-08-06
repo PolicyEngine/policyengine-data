aabee8af5ab5ca71ef223b59594b76ea88864e22
juaristi22 <juaristi@uni.minerva.edu>
2025-08-06 16:25:49 +0200

    lint

--- src/policyengine_data/calibration/__init__.py
+++ src/policyengine_data/calibration/__init__.py
@@ -1,2 +1,5 @@
-from .metrics_matrix_creation import create_metrics_matrix, validate_metrics_matrix
+from .metrics_matrix_creation import (
+    create_metrics_matrix,
+    validate_metrics_matrix,
+)
 from .target_rescaling import download_database, rescale_calibration_targets
